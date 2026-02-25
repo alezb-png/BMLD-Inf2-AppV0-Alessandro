50d0aabd8dbe0f64faa8c9b0286c863e67df73ee
alezb-png <zandtale@students.zhaw.ch>
2026-02-25 20:16:52 +0100

    Abstand geändert

--- views/home.py
+++ views/home.py
@@ -7,7 +7,8 @@ st.title("Meine erste Streamlit App")
 """
 Diese App wurde von folgenden Personen entwickelt:
 - Sara Durrer (Durrersar@students.zhaw.ch)
-- Alessandro Zandt (zandtale@students.zhaw.ch)
+- Alessandro Zandt (Zandtale@students.zhaw.ch)
+
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
 Autor: Samuel Wehrli (wehs@zhaw.ch)
