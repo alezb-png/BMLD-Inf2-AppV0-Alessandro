de686095301e2cc4fab5b860e641db7b80f21710
alezb-png <zandtale@students.zhaw.ch>
2026-03-25 12:19:39 +0100

    change

--- views/pH-Rechner_views.py
+++ views/pH-Rechner_views.py
@@ -129,9 +129,13 @@ else:  # pH → Konzentration
         
         st.session_state['data_df'] = pd.concat([st.session_state['data_df'], pd.DataFrame([result])])
 
-    # --- CODE UPDATE: save data to data manager ---
-    data_manager = DataManager()
-    data_manager.save_user_data(st.session_state['data_df'], 'data.csv')
-    # --- END OF CODE UPDATE ---
-
+# --- CODE UPDATE: save data to data manager ---
+data_manager = DataManager()
+data_manager.save_user_data(st.session_state['data_df'], 'data.csv')
+# --- END OF CODE UPDATE ---
 st.dataframe(st.session_state['data_df'])
+
+# --- CODE UPDATE: save data to data manager ---
+data_manager = DataManager()
+data_manager.save_user_data(st.session_state['data_df'], 'data.csv')
+# --- END OF CODE UPDATE ---
